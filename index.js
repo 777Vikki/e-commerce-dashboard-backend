88ed962fb9ed120c4bae30af6d1b4835e5c96a9d
Vivek <vkmm777@gmail.com>
2025-01-19 17:56:26 +0530

    clean up code and add validation on login and register api

--- index.js
+++ index.js
@@ -10,22 +10,57 @@ app.use(cors());
 
 app.post("/register", async (req, res) => {
     const user = new User(req.body);
-    let result = await user.save();
-    result = result.toObject();
-    delete result.password; 
-    res.send(result)
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if(user.name && user.name.trim() && user.password && user.password.trim() && user.email && emailRegex.test(user.email)) {
+        let result = await user.save();
+        result = result.toObject();
+        delete result.password;
+        const responseResult = {
+            result: true,
+            message: 'successful',
+            messageType: 'SUCCESS',
+            data: [result]
+        }
+        res.send(responseResult);
+    } else {
+        const responseResult = {
+            result: false,
+            data: [],
+            messageType: 'EMPTY_FIELD',
+            message: 'Either email or password or name are not available',
+        }
+        res.send(responseResult);
+    }
 });
 
 app.post("/login", async (req, res) => {
-    let user = await User.findOne(req.body).select("-password");
     if(req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password");
         if(user) {
-            res.send(user);
+            const responseResult = {
+                result: true,
+                message: 'successful',
+                messageType: 'SUCCESS',
+                data: [user]
+            }
+            res.send(responseResult);
         } else {
-            res.send('No user found.');
+            const responseResult = {
+                result: false,
+                data: [],
+                messageType: 'NO_USER',
+                message: 'No user found.',
+            }
+            res.send(responseResult);
         }
     } else {
-        res.send('No user found.');
+        const responseResult = {
+            result: false,
+            data: [],
+            messageType: 'EMPTY_FIELD',
+            message: 'Either email or password are not available',
+        }
+        res.send(responseResult);
     }
     
 });
